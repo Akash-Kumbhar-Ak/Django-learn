5c91c702ee18c2bd7a8cddc0afda8ce964c66a00
Akash Kumbhar <akashkumbhar3112@gmail.com>
2026-01-10 16:24:56 +0530

    django shell

--- core/home/models.py
+++ core/home/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    # id=models.AutoField(primary_key=True) add the django automatically
+    name=models.CharField(max_length=100)
+    age=models.IntegerField()
+    email=models.EmailField()
+    address=models.TextField()
+    image=models.ImageField()
+    city=models.CharField(max_length=100)
+
+class Product(models.Model):
+    pass
